f970d44597bc7c05c25a90a07723b962a42022b3
Miroslav Tanasic <tanasicmiroslav@gmail.com>
2020-07-30 13:06:13 +0200

    ProductionCalculator fix

--- CloudDERMS/CalculateEngineApp/CloudCommon/CalculateEngine/IProductionCalculator.cs
+++ CloudDERMS/CalculateEngineApp/CloudCommon/CalculateEngine/IProductionCalculator.cs
@@ -5,16 +5,22 @@ using DERMSCommon.WeatherForecast;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CloudCommon.CalculateEngine
 {
+    [ServiceContract]
     public interface IProductionCalculator
     {
+        [OperationContract]
         DerForecastDayAhead CalculateGenerator(Forecast forecast, Generator generator, Dictionary<long, DerForecastDayAhead> GeneratorForecastList);
+        [OperationContract]
         DerForecastDayAhead CalculateSubstation(Forecast forecast, Substation substation, NetworkModelTransfer networkModel, Dictionary<long, DerForecastDayAhead> GeneratorForecastList, Dictionary<long, DerForecastDayAhead> SubstationsForecast);
+        [OperationContract]
         DerForecastDayAhead CalculateSubRegion(SubGeographicalRegion subGeographicalRegion, NetworkModelTransfer networkModel, Dictionary<long, DerForecastDayAhead> SubstationsForecast, Dictionary<long, DerForecastDayAhead> SubGeographicalRegionsForecast);
+        [OperationContract]
         DerForecastDayAhead CalculateGeoRegion(GeographicalRegion geographicalRegion, NetworkModelTransfer networkModel, Dictionary<long, DerForecastDayAhead> SubGeographicalRegionsForecast);
 
 
